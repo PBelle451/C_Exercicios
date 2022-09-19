25afdbb0fb2d493b1a7cbda6ececf207e9dfc954
Pedro Belle <99338512+PBelle451@users.noreply.github.com>
2022-09-19 11:24:16 -0300

    Add files via upload

--- Soma.c
+++ Soma.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 
 int main(){
-	int x, y, soma;
-	printf("ESCREVA O VALOR DE X: ");
-	scanf("%d", &x);
-	printf("ESCREVA O VALOR DE Y: ");
-	scanf("%d", &y);
-	soma = x + y;
-	printf("SOMA: %d", soma);
+	int final, i, soma = 0;
+	printf("Insira um numero final: ");
+	scanf("%d", &final);
+	for(i = 0; i <= final; i++){
+		soma = soma + i;
+	}
+	printf("Soma: %d", soma);	
+
 	return 0;
 }
