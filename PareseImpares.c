6d854a0a35bc0d16581180226611d955cc4a73ff
Pedro Belle <99338512+PBelle451@users.noreply.github.com>
2022-09-21 11:30:41 -0300

    Add files via upload

--- PareseImpares.c
+++ PareseImpares.c
@@ -4,17 +4,19 @@
 int main(){
 	float par, impar;
 	int array[10];
-	int n, i;
+	int i;
 	for(i=0;i < 10; i++){
 		printf("\nInsira o numero: ");
 		scanf("%d", &array[i]);
 		if(array[i] % 2 == 0){
-			array[i] * 3;
+			array[i] = array[i] * 3;
 		} else {
-			array[i] += 1;
+			array[i] = array[i] + 1;
 		}
 	}
-	printf("\nArray: %d", array);
+	for(i=0;i<10;i++){
+		printf("%d ", array[i]);
+	}
 	
 	return 0;
 }
